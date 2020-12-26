f0fdb6bb36b961b040e64cdb6838f20196275fed
Rohan Gupta <guptarohan369@gmail.com>
2020-12-26 17:29:31 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ const App = () => {
         <ScrollToTop />
         <Navbar></Navbar>
         <Switch>
-            <Route path='/' exact component={Home} />
             <Route path='/services' exact component={Services} />
             <Route path='/products' exact component={Products} />
             <Route path='/sign-up' exact component={SignUp} />
+            <Route path='/' exact component={Home} />
             <Redirect to='/' from='*' />
         </Switch>
         <Footer />
